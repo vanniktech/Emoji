b68c36a289fe57ca80a3427678e772732b3ac58f
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-02-03 22:34:46 +0100

    Clean up

--- emoji-one/src/main/java/com/vanniktech/emoji/one/EmojiOneProvider.java
+++ emoji-one/src/main/java/com/vanniktech/emoji/one/EmojiOneProvider.java
@@ -1,7 +1,6 @@
 package com.vanniktech.emoji.one;
 
 import android.support.annotation.NonNull;
-
 import com.vanniktech.emoji.EmojiProvider;
 import com.vanniktech.emoji.emoji.EmojiCategory;
 import com.vanniktech.emoji.one.category.ActivityCategory;
@@ -12,25 +11,22 @@ import com.vanniktech.emoji.one.category.ObjectsCategory;
 import com.vanniktech.emoji.one.category.PeopleCategory;
 import com.vanniktech.emoji.one.category.PlacesCategory;
 import com.vanniktech.emoji.one.category.SymbolsCategory;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class EmojiOneProvider implements EmojiProvider {
-    @Override
-    @NonNull
-    public Map<String, EmojiCategory> getCategories() {
-        final LinkedHashMap<String, EmojiCategory> result = new LinkedHashMap<>();
+public final class EmojiOneProvider implements EmojiProvider {
+  @Override @NonNull public Map<String, EmojiCategory> getCategories() {
+    final Map<String, EmojiCategory> result = new LinkedHashMap<>();
 
-        result.put("People", new PeopleCategory());
-        result.put("Nature", new NatureCategory());
-        result.put("Activity", new ActivityCategory());
-        result.put("Places", new PlacesCategory());
-        result.put("Symbols", new SymbolsCategory());
-        result.put("Objects", new ObjectsCategory());
-        result.put("Foods", new FoodsCategory());
-        result.put("Flags", new FlagsCategory());
+    result.put("People", new PeopleCategory());
+    result.put("Nature", new NatureCategory());
+    result.put("Activity", new ActivityCategory());
+    result.put("Places", new PlacesCategory());
+    result.put("Symbols", new SymbolsCategory());
+    result.put("Objects", new ObjectsCategory());
+    result.put("Foods", new FoodsCategory());
+    result.put("Flags", new FlagsCategory());
 
-        return result;
-    }
+    return result;
+  }
 }
