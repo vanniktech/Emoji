7a0ea659658a77bc630c3223dd62493d729a3277
Ruben Gees <rubengees7@googlemail.com>
2017-02-21 04:48:01 +0100

    First work on improving the EmojiHandler performance

--- emoji/src/main/java/com/vanniktech/emoji/EmojiHandler.java
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiHandler.java
@@ -2,33 +2,46 @@ package com.vanniktech.emoji;
 
 import android.content.Context;
 import android.text.Spannable;
+import android.util.SparseIntArray;
+
 import com.vanniktech.emoji.emoji.Emoji;
 
 final class EmojiHandler {
-  static void addEmojis(final Context context, final Spannable text, final int emojiSize) {
-    final EmojiSpan[] spans = text.getSpans(0, text.length(), EmojiSpan.class);
+  private EmojiHandler() {
+    throw new AssertionError("No instances.");
+  }
 
-    for (final EmojiSpan oldSpan : spans) {
-      text.removeSpan(oldSpan);
+  static void addEmojis(final Context context, final Spannable text, final int emojiSize) {
+    final EmojiSpan[] existingSpans = text.getSpans(0, text.length(), EmojiSpan.class);
+    final SparseIntArray existingSpanRanges = new SparseIntArray();
+
+    for (final EmojiSpan span : existingSpans) {
+      final int spanStart = text.getSpanStart(span);
+      final int spanEnd = text.getSpanEnd(span);
+
+      existingSpanRanges.append(spanStart, spanEnd);
     }
 
     int i = 0;
-
-    final EmojiManager instance = EmojiManager.getInstance();
+    final EmojiManager emojiManager = EmojiManager.getInstance();
 
     while (i < text.length()) {
-      final Emoji found = instance.findEmoji(text.subSequence(i, text.length()));
+      final int existingSpanEnd = existingSpanRanges.get(i, -1);
 
-      if (found != null) {
-        text.setSpan(new EmojiSpan(context, found.getResource(), emojiSize), i, i + found.getLength(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-        i += found.getLength();
+      if (existingSpanEnd == -1) {
+        final Emoji found = emojiManager.findEmoji(text.subSequence(i, text.length()));
+
+        if (found != null) {
+          text.setSpan(new EmojiSpan(context, found.getResource(), emojiSize), i, i + found.getLength(),
+                  Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+          i += found.getLength();
+        } else {
+          i++;
+        }
       } else {
-        i++;
+        i += existingSpanEnd - i;
       }
     }
-  }
-
-  private EmojiHandler() {
-    throw new AssertionError("No instances.");
   }
 }
