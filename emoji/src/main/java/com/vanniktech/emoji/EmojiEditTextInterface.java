d0b7a1e1fc54faceb502941c19e36dfd206c0eb7
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2018-04-23 16:48:58 +0200

    Added interface to allow the creation of custom EmojiEditText that inherit from another class (like MultiAutoCompleteEditText)

---
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiEditTextInterface.java
@@ -0,0 +1,29 @@
+package com.vanniktech.emoji;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
+import android.support.annotation.CallSuper;
+import android.support.annotation.DimenRes;
+import android.support.annotation.Px;
+import android.support.v7.widget.AppCompatEditText;
+import android.widget.EditText;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import com.vanniktech.emoji.emoji.Emoji;
+
+/**
+ * Interface used to allow custom EmojiEditText objects on another project.
+ */
+public interface EmojiEditTextInterface {
+  float emojiSize = 1;
+
+  @CallSuper public void backspace();
+
+  @CallSuper public void input(final Emoji emoji);
+
+  public void setEmojiSize(@Px final int pixels);
+  public void setEmojiSize(@Px final int pixels, final boolean shouldInvalidate);
+  public void setEmojiSizeRes(@DimenRes final int res);
+  public void setEmojiSizeRes(@DimenRes final int res, final boolean shouldInvalidate);
+}
