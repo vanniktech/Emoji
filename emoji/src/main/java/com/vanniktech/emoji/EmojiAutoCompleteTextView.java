4c7cce0b29106cafcd05095f83e0cac5c8ce1866
Roc Boronat <roc@fewlaps.com>
2018-07-02 12:54:48 +0530

    Create AutoCompleteTextView using EditText as a base

---
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiAutoCompleteTextView.java
@@ -0,0 +1,104 @@
+package com.vanniktech.emoji;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
+import android.support.annotation.CallSuper;
+import android.support.annotation.DimenRes;
+import android.support.annotation.Px;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+
+import com.vanniktech.emoji.emoji.Emoji;
+
+public class EmojiAutoCompleteTextView extends AppCompatAutoCompleteTextView {
+    private float emojiSize;
+
+    public EmojiAutoCompleteTextView(final Context context) {
+        this(context, null);
+    }
+
+    public EmojiAutoCompleteTextView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+        if (!isInEditMode()) {
+            EmojiManager.getInstance().verifyInstalled();
+        }
+
+        final Paint.FontMetrics fontMetrics = getPaint().getFontMetrics();
+        final float defaultEmojiSize = fontMetrics.descent - fontMetrics.ascent;
+
+        if (attrs == null) {
+            emojiSize = defaultEmojiSize;
+        } else {
+            final TypedArray a = getContext().obtainStyledAttributes(attrs, com.vanniktech.emoji.R.styleable.EmojiAutoCompleteTextView);
+
+            try {
+                emojiSize = a.getDimension(com.vanniktech.emoji.R.styleable.EmojiAutoCompleteTextView_emojiSize, defaultEmojiSize);
+            } finally {
+                a.recycle();
+            }
+        }
+
+        setText(getText());
+    }
+
+    @Override
+    @CallSuper
+    protected void onTextChanged(final CharSequence text, final int start, final int lengthBefore, final int lengthAfter) {
+        EmojiManager.replaceWithImages(getContext(), getText(), emojiSize);
+    }
+
+    @CallSuper
+    public void backspace() {
+        final KeyEvent event = new KeyEvent(0, 0, 0, KeyEvent.KEYCODE_DEL, 0, 0, 0, 0, KeyEvent.KEYCODE_ENDCALL);
+        dispatchKeyEvent(event);
+    }
+
+    @CallSuper
+    public void input(final Emoji emoji) {
+        if (emoji != null) {
+            final int start = getSelectionStart();
+            final int end = getSelectionEnd();
+
+            if (start < 0) {
+                append(emoji.getUnicode());
+            } else {
+                getText().replace(Math.min(start, end), Math.max(start, end), emoji.getUnicode(), 0, emoji.getUnicode().length());
+            }
+        }
+    }
+
+    /**
+     * sets the emoji size in pixels and automatically invalidates the text and renders it with the new size
+     */
+    public final void setEmojiSize(@Px final int pixels) {
+        setEmojiSize(pixels, true);
+    }
+
+    /**
+     * sets the emoji size in pixels and automatically invalidates the text and renders it with the new size when {@code shouldInvalidate} is true
+     */
+    public final void setEmojiSize(@Px final int pixels, final boolean shouldInvalidate) {
+        emojiSize = pixels;
+
+        if (shouldInvalidate) {
+            setText(getText());
+        }
+    }
+
+    /**
+     * sets the emoji size in pixels with the provided resource and automatically invalidates the text and renders it with the new size
+     */
+    public final void setEmojiSizeRes(@DimenRes final int res) {
+        setEmojiSizeRes(res, true);
+    }
+
+    /**
+     * sets the emoji size in pixels with the provided resource and invalidates the text and renders it with the new size when {@code shouldInvalidate} is true
+     */
+    public final void setEmojiSizeRes(@DimenRes final int res, final boolean shouldInvalidate) {
+        setEmojiSize(getResources().getDimensionPixelSize(res), shouldInvalidate);
+    }
+}
