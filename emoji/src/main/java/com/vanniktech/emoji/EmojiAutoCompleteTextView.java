53c2b84690f796b804994a5fe8ac4db3aa1704c7
Roc Boronat <roc@fewlaps.com>
2018-07-02 17:23:47 +0530

    Indent with 2 spaces instead of 4, to keep project's style

--- emoji/src/main/java/com/vanniktech/emoji/EmojiAutoCompleteTextView.java
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiAutoCompleteTextView.java
@@ -13,92 +13,92 @@ import android.view.KeyEvent;
 import com.vanniktech.emoji.emoji.Emoji;
 
 public class EmojiAutoCompleteTextView extends AppCompatAutoCompleteTextView {
-    private float emojiSize;
+  private float emojiSize;
 
-    public EmojiAutoCompleteTextView(final Context context) {
-        this(context, null);
+  public EmojiAutoCompleteTextView(final Context context) {
+    this(context, null);
+  }
+
+  public EmojiAutoCompleteTextView(final Context context, final AttributeSet attrs) {
+    super(context, attrs);
+
+    if (!isInEditMode()) {
+      EmojiManager.getInstance().verifyInstalled();
     }
 
-    public EmojiAutoCompleteTextView(final Context context, final AttributeSet attrs) {
-        super(context, attrs);
+    final Paint.FontMetrics fontMetrics = getPaint().getFontMetrics();
+    final float defaultEmojiSize = fontMetrics.descent - fontMetrics.ascent;
 
-        if (!isInEditMode()) {
-            EmojiManager.getInstance().verifyInstalled();
-        }
+    if (attrs == null) {
+      emojiSize = defaultEmojiSize;
+    } else {
+      final TypedArray a = getContext().obtainStyledAttributes(attrs, com.vanniktech.emoji.R.styleable.EmojiAutoCompleteTextView);
 
-        final Paint.FontMetrics fontMetrics = getPaint().getFontMetrics();
-        final float defaultEmojiSize = fontMetrics.descent - fontMetrics.ascent;
-
-        if (attrs == null) {
-            emojiSize = defaultEmojiSize;
-        } else {
-            final TypedArray a = getContext().obtainStyledAttributes(attrs, com.vanniktech.emoji.R.styleable.EmojiAutoCompleteTextView);
-
-            try {
-                emojiSize = a.getDimension(com.vanniktech.emoji.R.styleable.EmojiAutoCompleteTextView_emojiSize, defaultEmojiSize);
-            } finally {
-                a.recycle();
-            }
-        }
-
-        setText(getText());
+      try {
+        emojiSize = a.getDimension(com.vanniktech.emoji.R.styleable.EmojiAutoCompleteTextView_emojiSize, defaultEmojiSize);
+      } finally {
+        a.recycle();
+      }
     }
 
-    @Override
-    @CallSuper
-    protected void onTextChanged(final CharSequence text, final int start, final int lengthBefore, final int lengthAfter) {
-        EmojiManager.replaceWithImages(getContext(), getText(), emojiSize);
+    setText(getText());
+  }
+
+  @Override
+  @CallSuper
+  protected void onTextChanged(final CharSequence text, final int start, final int lengthBefore, final int lengthAfter) {
+    EmojiManager.replaceWithImages(getContext(), getText(), emojiSize);
+  }
+
+  @CallSuper
+  public void backspace() {
+    final KeyEvent event = new KeyEvent(0, 0, 0, KeyEvent.KEYCODE_DEL, 0, 0, 0, 0, KeyEvent.KEYCODE_ENDCALL);
+    dispatchKeyEvent(event);
+  }
+
+  @CallSuper
+  public void input(final Emoji emoji) {
+    if (emoji != null) {
+      final int start = getSelectionStart();
+      final int end = getSelectionEnd();
+
+      if (start < 0) {
+        append(emoji.getUnicode());
+      } else {
+        getText().replace(Math.min(start, end), Math.max(start, end), emoji.getUnicode(), 0, emoji.getUnicode().length());
+      }
     }
+  }
 
-    @CallSuper
-    public void backspace() {
-        final KeyEvent event = new KeyEvent(0, 0, 0, KeyEvent.KEYCODE_DEL, 0, 0, 0, 0, KeyEvent.KEYCODE_ENDCALL);
-        dispatchKeyEvent(event);
+  /**
+   * sets the emoji size in pixels and automatically invalidates the text and renders it with the new size
+   */
+  public final void setEmojiSize(@Px final int pixels) {
+    setEmojiSize(pixels, true);
+  }
+
+  /**
+   * sets the emoji size in pixels and automatically invalidates the text and renders it with the new size when {@code shouldInvalidate} is true
+   */
+  public final void setEmojiSize(@Px final int pixels, final boolean shouldInvalidate) {
+    emojiSize = pixels;
+
+    if (shouldInvalidate) {
+      setText(getText());
     }
+  }
 
-    @CallSuper
-    public void input(final Emoji emoji) {
-        if (emoji != null) {
-            final int start = getSelectionStart();
-            final int end = getSelectionEnd();
+  /**
+   * sets the emoji size in pixels with the provided resource and automatically invalidates the text and renders it with the new size
+   */
+  public final void setEmojiSizeRes(@DimenRes final int res) {
+    setEmojiSizeRes(res, true);
+  }
 
-            if (start < 0) {
-                append(emoji.getUnicode());
-            } else {
-                getText().replace(Math.min(start, end), Math.max(start, end), emoji.getUnicode(), 0, emoji.getUnicode().length());
-            }
-        }
-    }
-
-    /**
-     * sets the emoji size in pixels and automatically invalidates the text and renders it with the new size
-     */
-    public final void setEmojiSize(@Px final int pixels) {
-        setEmojiSize(pixels, true);
-    }
-
-    /**
-     * sets the emoji size in pixels and automatically invalidates the text and renders it with the new size when {@code shouldInvalidate} is true
-     */
-    public final void setEmojiSize(@Px final int pixels, final boolean shouldInvalidate) {
-        emojiSize = pixels;
-
-        if (shouldInvalidate) {
-            setText(getText());
-        }
-    }
-
-    /**
-     * sets the emoji size in pixels with the provided resource and automatically invalidates the text and renders it with the new size
-     */
-    public final void setEmojiSizeRes(@DimenRes final int res) {
-        setEmojiSizeRes(res, true);
-    }
-
-    /**
-     * sets the emoji size in pixels with the provided resource and invalidates the text and renders it with the new size when {@code shouldInvalidate} is true
-     */
-    public final void setEmojiSizeRes(@DimenRes final int res, final boolean shouldInvalidate) {
-        setEmojiSize(getResources().getDimensionPixelSize(res), shouldInvalidate);
-    }
+  /**
+   * sets the emoji size in pixels with the provided resource and invalidates the text and renders it with the new size when {@code shouldInvalidate} is true
+   */
+  public final void setEmojiSizeRes(@DimenRes final int res, final boolean shouldInvalidate) {
+    setEmojiSize(getResources().getDimensionPixelSize(res), shouldInvalidate);
+  }
 }
