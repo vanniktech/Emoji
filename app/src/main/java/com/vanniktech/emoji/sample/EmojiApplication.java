0e38fd1acc8f23f2dc7e52a7cc4f1c59a95d5d63
Niklas Baudy <niklas.baudy@vanniktech.de>
2019-09-10 16:47:09 +0200

    Update dependencies.

--- app/src/main/java/com/vanniktech/emoji/sample/EmojiApplication.java
+++ app/src/main/java/com/vanniktech/emoji/sample/EmojiApplication.java
@@ -3,7 +3,6 @@ package com.vanniktech.emoji.sample;
 import android.app.Application;
 import android.os.StrictMode;
 import androidx.appcompat.app.AppCompatDelegate;
-import com.squareup.leakcanary.LeakCanary;
 import com.vanniktech.emoji.EmojiManager;
 import com.vanniktech.emoji.ios.IosEmojiProvider;
 
@@ -13,10 +12,6 @@ public class EmojiApplication extends Application {
   @Override public void onCreate() {
     super.onCreate();
 
-    if (LeakCanary.isInAnalyzerProcess(this)) {
-      return;
-    }
-
     AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_AUTO);
 
     EmojiManager.install(new IosEmojiProvider());
@@ -25,7 +20,5 @@ public class EmojiApplication extends Application {
       StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().build());
       StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().build());
     }
-
-    LeakCanary.install(this);
   }
 }
