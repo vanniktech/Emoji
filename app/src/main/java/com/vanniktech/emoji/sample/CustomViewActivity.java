990b951712de6d1e19c399e4968fcc12ae6f60ad
Niklas Baudy <niklas.baudy@vanniktech.de>
2019-10-23 15:27:22 +0200

    Add sample with custom view.

---
+++ app/src/main/java/com/vanniktech/emoji/sample/CustomViewActivity.java
@@ -0,0 +1,38 @@
+package com.vanniktech.emoji.sample;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.vanniktech.emoji.EmojiEditText;
+import com.vanniktech.emoji.EmojiPopup;
+
+public class CustomViewActivity extends AppCompatActivity {
+  @Override @SuppressLint("SetTextI18n") protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(new CustomView(this, null));
+  }
+
+  static class CustomView extends LinearLayout {
+    public CustomView(final Context context, @Nullable final AttributeSet attrs) {
+      super(context, attrs);
+      View.inflate(context, R.layout.view_custom, this);
+    }
+
+    @Override protected void onAttachedToWindow() {
+      super.onAttachedToWindow();
+
+      @SuppressLint("WrongViewCast") final EmojiEditText editText = findViewById(R.id.customViewEditText);
+
+      final EmojiPopup emojiPopup = EmojiPopup.Builder.fromRootView((View) getParent())
+          .build(editText);
+
+      editText.disableKeyboardInput(emojiPopup);
+    }
+  }
+}
