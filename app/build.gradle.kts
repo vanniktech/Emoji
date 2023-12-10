35838fec7703bfa05c37bf18103dbe3a02eac843
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-12-10 13:05:52 -0300

    Technical: Leverage Kotlin Build Scripts.

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+}
+
+kotlin {
+  jvmToolchain(11)
+}
+
+android {
+  val shouldSign = project.hasProperty("RISK_BATTLE_SIMULATOR_RELEASE_STORE_FILE") &&
+    project.hasProperty("RISK_BATTLE_SIMULATOR_RELEASE_STORE_PASSWORD") &&
+    project.hasProperty("RISK_BATTLE_SIMULATOR_RELEASE_KEY_ALIAS") &&
+    project.hasProperty("RISK_BATTLE_SIMULATOR_RELEASE_KEY_PASSWORD")
+
+  namespace = "com.vanniktech.emoji.sample"
+
+  compileSdk = libs.versions.compileSdk.get().toInt()
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+
+  defaultConfig {
+    applicationId = "com.vanniktech.emoji.sample"
+    minSdk = libs.versions.minSdk.get().toInt()
+    targetSdk = libs.versions.targetSdk.get().toInt()
+    versionCode = 1
+    versionName = project.property("VERSION_NAME").toString()
+
+    vectorDrawables.useSupportLibrary = true
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildFeatures {
+    viewBinding = true
+  }
+
+  signingConfigs {
+    if (shouldSign) {
+      create("release") {
+        storeFile = file(project.property("RISK_BATTLE_SIMULATOR_RELEASE_STORE_FILE").toString())
+        storePassword = project.property("RISK_BATTLE_SIMULATOR_RELEASE_STORE_PASSWORD").toString()
+        keyAlias = project.property("RISK_BATTLE_SIMULATOR_RELEASE_KEY_ALIAS").toString()
+        keyPassword = project.property("RISK_BATTLE_SIMULATOR_RELEASE_KEY_PASSWORD").toString()
+      }
+    }
+  }
+
+  buildTypes {
+    release {
+      signingConfigs.findByName("release")?.let { signingConfig = it }
+      isMinifyEnabled = true
+      isShrinkResources = true
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+    }
+  }
+}
+
+dependencies {
+  implementation(project(":emoji-ios"))
+  implementation(project(":emoji-google"))
+  implementation(project(":emoji-google-compat"))
+  implementation(project(":emoji-twitter"))
+  implementation(project(":emoji-facebook"))
+  implementation(project(":emoji-material"))
+  implementation(libs.timber)
+}
+
+dependencies {
+  debugImplementation(libs.leakcanary.android)
+}
+
+dependencies {
+  androidTestImplementation(libs.androidx.test.espresso)
+  androidTestImplementation(libs.androidx.test.ext)
+  androidTestImplementation(libs.androidx.test.rules)
+  androidTestImplementation(libs.falcon)
+  androidTestImplementation(libs.junitintegrationrules)
+  androidTestImplementation(libs.screengrab)
+  androidTestImplementation(libs.espressocoreutils)
+}
---
+++ emoji-google-compat/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+  id("org.jetbrains.dokka")
+  id("org.jetbrains.kotlin.multiplatform")
+  id("com.android.library")
+  id("org.jetbrains.kotlin.plugin.parcelize")
+  id("me.tylerbwong.gradle.metalava")
+  id("com.vanniktech.maven.publish")
+  id("app.cash.licensee")
+}
+
+licensee {
+  allow("Apache-2.0")
+}
+
+metalava {
+  filename.set("api/current.txt")
+  sourcePaths.setFrom("src/commonMain", "src/androidMain", "src/jvmMain")
+}
+
+kotlin {
+  applyDefaultHierarchyTemplate()
+
+  androidTarget {
+    publishLibraryVariants("release")
+  }
+//  ios("ios")
+  jvm()
+  jvmToolchain(11)
+
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        api(project(":emoji"))
+      }
+    }
+
+    val commonTest by getting {
+      dependencies {
+        implementation(libs.kotlin.test.common)
+        implementation(libs.kotlin.test.annotations.common)
+      }
+    }
+
+    val androidMain by getting {
+      dependencies {
+        api(libs.androidx.emoji.appcompat)
+      }
+    }
+
+    val androidUnitTest by getting {
+      dependencies {
+        implementation(libs.kotlin.test.junit)
+        implementation(libs.robolectric)
+      }
+    }
+
+    val jvmTest by getting {
+      dependencies {
+        implementation(libs.kotlin.test.junit)
+      }
+    }
+  }
+}
+
+android {
+  namespace = "com.vanniktech.emoji.googlecompat"
+
+  compileSdk = libs.versions.compileSdk.get().toInt()
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+
+  defaultConfig {
+    minSdk = libs.versions.minSdk.get().toInt()
+  }
+
+  resourcePrefix = "emoji_googlecompat_"
+}
+
+dependencies {
+  api(project(":emoji"))
+}
