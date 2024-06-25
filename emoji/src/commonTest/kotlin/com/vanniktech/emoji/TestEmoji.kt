7eecf083aee81a95ff22a692142a864e47ff469f
Niklas Baudy <niklas.baudy@vanniktech.de>
2024-06-25 12:19:58 +0700

    Technical: Use equals without referencing javaClass. (#1106)

--- emoji/src/commonTest/kotlin/com/vanniktech/emoji/TestEmoji.kt
+++ emoji/src/commonTest/kotlin/com/vanniktech/emoji/TestEmoji.kt
@@ -64,7 +64,7 @@ data class TestEmoji internal constructor(
 
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
-    if (javaClass != other?.javaClass) return false
+    if (other == null || this::class != other::class) return false
 
     other as TestEmoji
 
