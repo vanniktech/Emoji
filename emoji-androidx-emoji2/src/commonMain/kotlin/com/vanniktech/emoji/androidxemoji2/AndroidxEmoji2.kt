47cacd5fd757e45eac2932a946eba4a79d1bea31
Niklas Baudy <niklas.baudy@vanniktech.de>
2024-06-25 12:07:44 +0700

    Technical: Use equals without referencing javaClass.

--- emoji-androidx-emoji2/src/commonMain/kotlin/com/vanniktech/emoji/androidxemoji2/AndroidxEmoji2.kt
+++ emoji-androidx-emoji2/src/commonMain/kotlin/com/vanniktech/emoji/androidxemoji2/AndroidxEmoji2.kt
@@ -40,7 +40,7 @@ internal class AndroidxEmoji2 internal constructor(
 
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
-    if (javaClass != other?.javaClass) return false
+    if (other == null || this::class != other::class) return false
 
     other as AndroidxEmoji2
 
